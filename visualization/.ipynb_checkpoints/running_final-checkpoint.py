1c968c8e0d8c1c73124ae9975059d7d86f26629a
alisonands <131694547+alisonands@users.noreply.github.com>
2023-12-28 23:54:13 -0500

    updates

--- visualization/.ipynb_checkpoints/running_final-checkpoint.py
+++ visualization/.ipynb_checkpoints/running_final-checkpoint.py
@@ -26,46 +26,90 @@ pn.extension('tabulator')
 import hvplot.pandas
 
 
-# In[2]:
+# In[18]:
 
 
 #format: year, country, gender, age, time
 #(all participants)
 berlin_marathon = pd.read_csv('1_running_datasets/berlin_marathon_1974_2023.csv')
 
+age_null_values = ['H', 'JU20', 'L1', 'L2', 'L', 'L3', 'D1', 'L4', 'D3', 'D2',
+                  'BM','A', 'B', 'C', 'DH', 'DA', 'DB', 'DJ', 'M0', 'Ber', 'M',
+                  'M<', 'Jug']
+
 #data cleaning
-berlin_marathon = berlin_marathon[(berlin_marathon['time'] != '–')&
-                                  (berlin_marathon['time'] != 'DSQ')&
-                                  (berlin_marathon['time'] != 'no time')]
+berlin_marathon = berlin_marathon[~(berlin_marathon['time'].isin(['–', 'DSQ', 'no time']))&
+                                  ~(berlin_marathon['age'].isin(age_null_values))]
 #convert time to hrs
 berlin_marathon['time_hrs'] = pd.to_timedelta(berlin_marathon['time']).dt.total_seconds() / 3600
 berlin_marathon.dropna(subset = 'time_hrs', how='any')
-berlin_marathon
+berlin_marathon['age'] = pd.to_numeric(berlin_marathon['age'])
 
 
-# In[3]:
+# In[22]:
 
 
 year_slider = pn.widgets.IntSlider(name='Year slider', start=1974, end=2023, step=1, value=2023)
 idf = berlin_marathon.interactive()
 
 
-# ##### Average and Finishing times
+# ## Average and Finishing times
 # - How have average times changed over the years?
 #     - Overall average time has increased. Likely due to the increase in interest in the sport.
 # - How has the fastest time changed over the years?
 #     - Finishing times have decreased. obvs. because records are meant to be broken.
 
-# In[34]:
+# In[4]:
 
 
 overall_times = pd.DataFrame(berlin_marathon.groupby('year')['time_hrs'].mean().rename('Average Times'))#.hvplot(kind = 'scatter', title = 'Average times over the years')
 overall_times['Finishing times'] = berlin_marathon.groupby('year')['time_hrs'].min()
-overall_times_plot = overall_times.hvplot(title = 'Average and Finishing times')
+overall_times_plot = overall_times.hvplot(title = 'Average and Finishing times', xlabel = 'year', ylabel = 'time(hrs)')
 overall_times_plot
 
 
-# ##### Where are most runners from?
+# ## Table df
+
+# In[5]:
+
+
+marathon_table = idf.drop(columns = ['time']).pipe(pn.widgets.Tabulator, pagination='remote', page_size = 10, sizing_mode='stretch_width') 
+marathon_table
+
+
+# ## How does gender affect running times?
+
+# In[20]:
+
+
+berlin_marathon.dtypes
+
+
+# In[58]:
+
+
+age_data_pipeline = (
+    idf[
+        age_ranges[age_selector.value][0] <= 
+        idf['age'] < 
+        age_ranges[age_selector.value][1]
+    ]
+    .groupby(['age'])['time_hrs'].mean()
+    .sort_values(ascending = False)
+    .to_frame()
+    .reset_index()
+    .reset_index(drop=True)
+
+)
+age_data_plot = gender_data_pipeline.hvplot(
+    kind = 'scatter',
+    title = 'Average time by age', 
+    x = 'age',
+    y = 'time_hrs')
+age_data_plot
+
+
+# ## Where are most runners from?
 # - Where do most runners come from?
 #     - Germany
 # - Where are the shortest times from?
@@ -73,7 +117,7 @@ overall_times_plot
 # - Are these two places the same?
 #     - no. no they are not.
 
-# In[5]:
+# In[7]:
 
 
 runners_count = berlin_marathon.groupby(['country', 'gender']).size().sort_values(ascending = False)
@@ -85,14 +129,16 @@ runners_count_plot = runners_count.hvplot(kind = 'bar',
 runners_count_plot
 
 
-# In[22]:
+# ## Where are the top 50 runners from?
+
+# In[8]:
 
 
 gender_selector = pn.widgets.Select(options=['All', 'male', 'female'], value='Men', name='Select Gender')
 gender_selector
 
 
-# In[32]:
+# In[9]:
 
 
 gender_selector = pn.widgets.Select(options=['male', 'female'], value='Men', name='Select Gender')
@@ -122,7 +168,7 @@ berlin_marathon_plot = berlin_marathon_.hvplot(kind = 'bar',
                                                title = 'Where are the top 50 runners from?')
 berlin_marathon_plot
 
-# In[56]:
+# In[10]:
 
 
 plot_selector = pn.widgets.Select(options=['Top 50', 'Runner Population'], name='Select Plot')
@@ -134,24 +180,22 @@ def reactive_plot(selected_plot):
         return runners_count_plot
 
 
-# In[61]:
+# In[59]:
 
 
 template = pn.template.FastListTemplate(
     title = 'Berlin Marathon runners',
-    sidebar = [pn.pane.Markdown("# Runners!"),
+    sidebar = [pn.pane.Markdown("# Berlin Marathon!"),
+               [pn.pane.Markdown("#### ")],
                year_slider
               ],
-    main = [pn.Row(pn.Column(overall_times_plot)),#, top_50_plot)),
-           pn.Row(pn.Column(plot_selector, reactive_plot))]
+    main = [pn.Row(pn.Column(overall_times_plot), pn.Column(marathon_table)),#, top_50_plot)),
+           pn.Row(pn.Column(age_data_plot), pn.Column(plot_selector, reactive_plot))]
 )
 template.servable();
 
 
-# In[59]:
-
-
-reactive_plot('Plot 2')
+# In[12]:
 
 
 # In[ ]:
